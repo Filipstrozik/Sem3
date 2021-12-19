4870de7b1a669905a5ea0fe599ba8a12ff519eaa
Filip Strózik <47420153+Filipstrozik@users.noreply.github.com>
2021-12-19 20:51:35 +0100

    Add files via upload

---
+++ ProjektTestowy/CTable.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class CTable {
+public:
+	static const string DEFAULT_NAME;
+	static const int DEFAULT_SIZE;
+
+
+	CTable();
+	CTable(string sName, int iTableLen);
+	CTable(const CTable& pcOther);
+	~CTable();
+
+	CTable operator+(const CTable &pcOther);
+	CTable& operator=(const CTable &pcOther);
+	CTable& operator--(int);
+	
+	void vSetName(string sName);
+	string sGetName();
+	bool bSetNewSize(int iTableLen);
+	int iGetSize();
+	//void vEnsureCapacity(const int& minCapacity);
+	//void vAdd(const int& elem);
+	void vFill();
+	void vClearElements();
+	bool bSetValueAt(int iOffset, int iNewVal);
+	void vPrint();
+	CTable* pcClone() const;
+
+private:
+	string s_name;
+	int i_table_length;
+	//int i_capacity;
+	int* pi_table;
+};
+
